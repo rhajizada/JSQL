d8c2abfd72d4c9aeedb5378ede4f79153f9e6f1f
rhajizada <vice.genius@gmail.com>
2019-05-24 05:30:15 -0400

    Now can insert into table and export as html

--- jsql.js
+++ jsql.js
@@ -27,30 +27,38 @@ Array.prototype.equals = function (array) {
 }
 // Hide method from for-in loops
 Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+// Dependencies
+jsonfile = require('jsonfile');
+fs = require('fs');
 
 module.exports =  class Table {
-    constructor(filename) {
-        this.jsonfile = require('jsonfile');
+    constructor(filename, name) {
+        // Constructs a table object from JSON file given
+        this.name = name;
         this.filename = filename;
+        // Reads JSON file
         try{
-            this.table = this.jsonfile.readFileSync(filename);
+            this.table = jsonfile.readFileSync(filename);
         }
         catch(e){
-            console.warn("Error reading " + filename);
+            throw new Error("Error reading " + filename);
         }
+        // Prints that table is initialized succes
         if (this.table.length != 0) {
             console.log('Table ' + this.filename + ' succesfully initialized');
         }
+        // Creates table from first object in array
         this.schema = [];
         for (var i = 0; i < Object.keys(this.table[0]).length; i++) {
             this.schema.push(Object.keys(this.table[0])[i]);
         }
+        // Checks correctness of schema
         this.schemaIsCorrect = this.checkSchema();
         if(this.schemaIsCorrect){
             console.log("Schema is consistent throughout the file");
         }
         else{
-            console.warn("Schema is not consistent throughtout the file");
+            throw new Error("Schema is not consistent throughtout the file");
         }
     }
 
@@ -71,25 +79,95 @@ module.exports =  class Table {
 
     printSchema() {
         // Prints the table schema 
+        console.log(this.name + " schema is:");
         var schema = '';
         for (var i = 0; i < Object.keys(this.table[0]).length; i++) {
-            schema = schema + Object.keys(this.table[0])[i] + "||";
+            schema = schema + Object.keys(this.table[0])[i] + "\t";
         }
         console.log(schema);
     }
 
-    printTable() {
-        var tuple = [];
+    print() {
+        // Prints the whole table on the console
         for (var i = 0; i < this.table.length; i++) {
+            var tuple = "";
             for (var j = 0; j < this.schema.length; j++) {
-                tuple[i] += "\t" + this.table[i][this.schema[j]] + "\t";
+                tuple += this.table[i][this.schema[j]] + "\t";
             }
-            tuple[i] = tuple[i].substr(10);
-            console.log(tuple[i]);
+            console.log(tuple);
         }
     }
-    
+
+    toString(){
+        // Returns the table as a string
+        var tableAsString = "";
+        for (var i = 0; i < this.table.length; i++) {
+            var tuple = "";
+            for (var j = 0; j < this.schema.length; j++) {
+                tuple += this.table[i][this.schema[j]] + "\t";
+            }
+            tableAsString += tuple;
+            tableAsString += "\n";
+        }
+        return tableAsString;
+    }
+
+
+    insert(row){
+        // Given the object inserts it into the table and also adds it to the file
+        var rowSchema = [];
+        for(var i = 0; i < Object.keys(row).length; i++){
+            rowSchema.push(Object.keys(row)[i]);
+        }
+        var rowSchemaCorrect = rowSchema.equals(this.schema);
+        if(rowSchemaCorrect){
+            this.table.push(row);
+            let updatedTable = JSON.stringify(this.table);
+                fs.writeFile(this.filename, updatedTable, (err) => {
+                    if (err) console.log(err);
+                  });
+        }
+        else{
+            console.warn("Input object is using wrong schema.\nFailed inserting into table.\nTable schema is: ");
+            console.log(this.schema);
+            console.warn("Your input object schema is: ");
+            console.log(rowSchema);
+        }
+    }
+
+    toHTML(){
+        // Writes table into a new html file for better visualization
+        var html = "<!DOCTYPE html>\n";
+        html += "<html>\n<head>\n<style>\n";
+        html += "table {\n\tfont-family: arial, sans-serif;\n\ttext-align: center;\n\tborder-collapse: collapse;\n\twidth: 100%;\n}\n";
+        html += "td, th {\n\tborder: 1px solid #dddddd;\n\ttext-align: center;\n\ttext-align: left;\n\tpadding: 8px;\n}\n";
+        html += "tr:nth-child(even) {\n\tbackground-color: #dddddd;\n}\n";
+        html += "body\n{\n\ttext-align: center;\n}\n"
+        html += "</style>\n</head>\n<body>\n<h2>";
+        html += this.name;
+        html += "</h2>\n<table>\n<tr>";
+        for(var i = 0; i < this.schema.length; i++){
+            let element = "<th>" + this.schema[i] + "</th>\n";
+            html += element;
+        }
+        html += "</tr>\n";
+        for(i in this.table){
+            html += "<tr>";
+            for(var j = 0; j <  Object.values(this.table[i]).length; j++){
+                let element = "<th>" + Object.values(this.table[i])[j] + "</th>";
+                html += element;
+            }
+            html += "</tr>";
+        }
+        html +="</table>\n</body>\n</html>"
+        fs.writeFile(this.name +".html", html, (err) => {
+            if (err) console.warn(err);
+            else console.log("HTML file of table created");
+          });
+    }
+
     simpleSearch(column, value) {
+        // Looks for rows that match and adds them to return array of objects
         var result = [];
         for (var i = 0; i < this.table.length; i++) {
             if (this.table[i][column] == value) {
@@ -99,11 +177,12 @@ module.exports =  class Table {
         if (result.length != 0) {
             return result;
         } else {
-            console.log("Value not found!");
+            console.warn("Value not found!");
         }
     }
     
     simpleSearchWithAttribute(column, value, attribute){
+        // Looks for rows that match and adds value of the attribute to return array
         var result = [];
         for (var i = 0; i < this.table.length; i++) {
             if (this.table[i][column] == value) {
@@ -116,6 +195,5 @@ module.exports =  class Table {
             console.log("Value not found!");
         }
     }
-
 };
 
--- test.js
+++ test.js
@@ -1,8 +1,26 @@
 let Table = require('./jsql');
-let table = new Table('data.json');
+let table = new Table('song.json', 'Led Zeppelin I');
+// table.printSchema();
+// table.printTable();
+// let result = table.simpleSearch('Ping', 49);
+// let resultWithAttribute = table.simpleSearchWithAttribute('Date', '2017-11-16', 'ID');
+// console.log(result);
+// console.log(resultWithAttribute);
+// table.printTable();
 table.printSchema();
-table.printTable();
-let result = table.simpleSearch('Ping', 49);
-let resultWithAttribute = table.simpleSearchWithAttribute('Date', '2017-11-16', 'ID');
-console.log(result);
-console.log(resultWithAttribute);
+console.log(table.toString());
+console.dir(table.simpleSearch('Song', 'Dazed and Confused'));
+
+
+// let newRow = {
+//     ID: 44,
+//     Date: '2019-05-23',
+//     Time: '20:49:01',
+//     Download: 3.01,
+//     Upload: 5.02,
+//     Ping: 20
+// };
+
+// table.insert(newRow);
+// table.toHTML();
+// table.printTable();
